cfbb0f7f4833662b3a286bd52b2b22876b2b0d66
maestroi <meastro@protonmail.com>
2023-03-24 23:37:06 +0100

    Add docker-compose and config

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Nimiq installer
 
 This repo is to have a automated installer for a nimiq validator or RPC node with full history.
+
+
+# Oneliner
+Be aware executing things directly in terminal as root, check the files first!
+
+curl -s https://raw.githubusercontent.com/maestroi/nimiq-installer/master/install_protocol.sh | bash -s
+
+# Use cloud-config
+You can use the cloud config for any populair cloud provider to use for startup any node
--- install_protocol.sh
+++ install_protocol.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Set default values
+username=${1:-protocol}
+docker_group=${2:-docker}
+
 # Check if the script is running as root
 if [[ $(id -u) -ne 0 ]]; then
     echo "This script must be run as root."
@@ -12,20 +16,43 @@ if [[ $(lsb_release -si) != "Ubuntu" ]]; then
     exit 1
 fi
 
-# Create the user called protocol
-useradd -m protocol
+# Check if the user protocol exists, and create it if it doesn't
+if ! id -u $username > /dev/null 2>&1; then
+    useradd -m $username
+fi
 
 # Update and upgrade Ubuntu
 apt-get update && apt-get upgrade -y
 
+# Check if the protocol user is already in the docker group, and add it if it's not
+if ! id -nG $username | grep -qw $docker_group; then
+    usermod -aG $docker_group $username
+fi
+
 # Install Docker and Docker Compose
 apt-get install -y docker.io docker-compose
 
-# Add the protocol user to the docker group
-usermod -aG docker protocol
-
 # Install some common packages
 apt-get install -y curl git vim
+
+# Check if the directories already exist, and create them if they don't
+if [ ! -d "/opt/nimiq/configuration" ]; then
+    mkdir -p /opt/nimiq/configuration
+fi
+
+if [ ! -d "/opt/nimiq/data" ]; then
+    mkdir -p /opt/nimiq/data
+fi
+
+if [ ! -d "/opt/nimiq/secrets" ]; then
+    mkdir -p /opt/nimiq/secrets
+fi
+
+# Set permissions for the directories
+chown -R $username:$username /opt/nimiq/configuration /opt/nimiq/data /opt/nimiq/secrets
+chmod -R 750 /opt/nimiq/configuration
+chmod -R 755 /opt/nimiq/data
+chmod -R 740 /opt/nimiq/secrets
 
 # Print a message indicating that the script has finished
 echo "The script has finished."
