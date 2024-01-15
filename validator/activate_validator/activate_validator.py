f08d6a3afa627f9561361b2d830ecbe1a627a293
maestroi <meastro@protonmail.com>
2024-01-15 18:16:57 +0100

    Some fixes

--- validator/activate_validator/activate_validator.py
+++ validator/activate_validator/activate_validator.py
@@ -117,7 +117,8 @@ def get_tx(tx_hash):
     logging.info(f"Transaction: {res}")
 
 def push_raw_tx(tx_hash):
-    res = nimiq_request("pushRawTransaction", [tx_hash])
+    res = nimiq_request("pushTransaction", [tx_hash])
+    logging.info(f"Transaction: {res}")
     if res is None:
         return None
     if 'error' in res:
