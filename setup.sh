d2c8738e733fb1d2044e313486da38348ab4e0a0
maestroi <meastro@protonmail.com>
2024-06-03 00:48:42 +0200

    feat(): Add history + 24.04 support

--- setup.sh
+++ setup.sh
@@ -79,7 +79,7 @@ function setup_firewall() {
 function install_docker() {
     echo -e "${GREEN}Installing Docker...${NC}"
     apt-get update &>/dev/null
-    apt-get install ca-certificates curl &>/dev/null
+    apt-get install -y ca-certificates curl &>/dev/null
     install -m 0755 -d /etc/apt/keyrings &>/dev/null
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc &>/dev/null
     chmod a+r /etc/apt/keyrings/docker.asc &>/dev/null
@@ -88,7 +88,7 @@ function install_docker() {
         $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
         sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     apt-get update &>/dev/null
-    apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &>/dev/null
+    apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin &>/dev/null
     docker network create app_net &>/dev/null
     if ! id -nG $username | grep -qw docker; then
         echo -e "${GREEN}Adding user $username to the docker group.${NC}"
